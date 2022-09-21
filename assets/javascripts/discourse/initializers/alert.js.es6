997612a5d3c0aa3168ddbfd59be7ff34657a2337
shadrach-tayo <shadrachtemitayo@gmail.com>
2022-09-21 22:35:59 +0100

    configure plugin, add to a badges plugin outlet, add config settings

---
+++ assets/javascripts/discourse/initializers/alert.js.es6
@@ -0,0 +1,7 @@
+export default {
+  name: "alert",
+  initialize() {
+    // alert("alert boxes are annoying!");
+    console.log('debug plugin')
+  },
+};
--- plugin.rb
+++ plugin.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
-# name: discourse-plugin-name
-# about: TODO
+# name: desoc-badges
+# about: A discourse plugin to display users desoc badges
 # version: 0.0.1
-# authors: Discourse
-# url: TODO
+# authors: Desci Labs
+# url: https://desci.com
 # required_version: 2.7.0
 
-enabled_site_setting :plugin_name_enabled
+enabled_site_setting :desoc_badges_enabled
 
 after_initialize do
 end
